4902438006a17b869a4d0af90f072283f00853d7
SobamboS <sobamboolusola1152@gmail.com>
2023-04-11 11:51:24 +0100

    hellozzzzzzzz

---
+++ src/main/java/com/millie/millietransportsystemuberclone/MillieTransportSystemUberCloneApplication.java
@@ -0,0 +1,13 @@
+package com.millie.millietransportsystemuberclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MillieTransportSystemUberCloneApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(MillieTransportSystemUberCloneApplication.class,args);
+    }
+
+}
---
+++ src/test/java/com/millie/millietransportsystemuberclone/MillieTransportSystemUberCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.millie.millietransportsystemuberclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillieTransportSystemUberCloneApplicationTests{
+
+    @Test
+    void contextLoads( ){
+    }
+
+}
